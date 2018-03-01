d194661601dc4db048c9b49d1a07491322456ab2
David Sarginson <david.sarginson@digital.hmrc.gov.uk>
2018-03-01 12:45:35 +0000

    adding bootstrap styling

--- src/cljs/acro_buddy/home.cljs
+++ src/cljs/acro_buddy/home.cljs
@@ -7,11 +7,11 @@
 (def results (r/atom []))
 
 (defn acronym-list []
-  [:div
+  [:div.row
    (for [{:keys [name description]} @results]
-     ^{:key name} [:div
-                   [:span.acroName name]
-                   [:span.acroDesc description]])])
+     ^{:key name} [:div.row
+                   [:div.col-md-3 name]
+                   [:div.col-md-9 description]])])
 
 (defn handle-search [criteria]
   (let [acronym (trim (:acronym @criteria))]
@@ -23,17 +23,18 @@
 (defn search-form []
   (let [criteria (r/atom {:acronym ""})]
     (fn []
-      [:div
-       [:input {:type "text"
-                :placeholder "Search Hackronym here"
-                :style {:width "20%"}
-                :value (:acronym @criteria)
-                :on-change #(swap! criteria assoc :acronym (-> %
-                                                               .-target
-                                                               .-value))}]
-       [:input {:type "button"
-                :value "search"
-                :on-click #(handle-search criteria)}]
+      [:div.row
+       [:div.col-md-4.offset-md-2
+        [:input.form-control {:type "text"
+                              :placeholder "Search Hackronym"
+                              :value (:acronym @criteria)
+                              :on-change #(swap! criteria assoc :acronym (-> %
+                                                                             .-target
+                                                                             .-value))}]]
+       [:div.col-md-4
+        [:input.btn.btn-primary {:type "button"
+                                 :value "search"
+                                 :on-click #(handle-search criteria)}]]
        [acronym-list]])))
 
 
